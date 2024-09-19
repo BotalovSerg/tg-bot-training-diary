55341dd7c89faddcd1c89e303f3330f697de893a
botalov <89090168690@mail.ru>
2024-09-19 13:56:54 +0500

    added models Scheme Category

--- bot/database/models.py
+++ bot/database/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, Text, BigInteger, DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, Text, BigInteger, DateTime, func, ForeignKey
 
 
 class Base(DeclarativeBase):
@@ -19,3 +19,21 @@ class Account(Base):
     age: Mapped[str | None] = mapped_column(nullable=True)
     bio: Mapped[str | None] = mapped_column(Text, nullable=True)
     sum_workouts: Mapped[int] = mapped_column(default=0)
+
+
+class Scheme(Base):
+    __tablename__ = "scheme_training"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(30))
+    content: Mapped[str] = mapped_column(Text)
+    category_id: Mapped[int] = mapped_column(ForeignKey("category_scheme_training.id"))
+    category: Mapped["CategoryScheme"] = relationship(back_populates="schemes")
+
+
+class CategoryScheme(Base):
+    __tablename__ = "category_scheme_training"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(30))
+    schemes: Mapped[list["Scheme"]] = relationship(back_populates="category")
